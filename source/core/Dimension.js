f99376bad3c578e890c766152477821c53898dbc
ToxesFoxes <toxes_foxes@outlook.com>
2020-09-14 01:25:55 +0600

    Added dish model and some changes

--- source/core/Dimension.js
+++ source/core/Dimension.js
@@ -5,10 +5,11 @@ IDRegistry.genItemID("red_pellet")
 Item.createFoodItem("blue_pellet", "Blue pellet", { name: "blue_pellet" }, { stack: 8, food: 4 })
 Item.createFoodItem("red_pellet", "Red pellet", { name: "red_pellet" }, { stack: 8, food: 4 })
 function PlayerTransfer(dim) {
-    if (dim == 1200) {
-        Matrix.time = World.getWorldTime()
+    if (dim == 1997) {
+        Matrix.data.time = World.getWorldTime()
     } else {
-        World.setWorldTime(Matrix.time)
+        if (Matrix.data.time != 0 && Player.getDimension() != 0)
+            World.setWorldTime(Matrix.data.time)
     }
     Dimensions.transfer(Player.get(), dim)
     if (Player.getPosition().y < 5)
@@ -38,12 +39,12 @@ Callback.addCallback("FoodEaten", function () {
         case ItemID.red_pellet:
             Entity.addEffect(Player.get(), Native.PotionEffect.regeneration, 5, 12000)
             Entity.addEffect(Player.get(), Native.PotionEffect.damageResistance, 2, 12000)
-            PlayerTransfer(1200)
+            PlayerTransfer(1997)
             break
     }
 })
 
-var MatrixDimension = new Dimensions.CustomDimension("Matrix", 1200)
+var MatrixDimension = new Dimensions.CustomDimension("Matrix", 1997)
 var generator = new Dimensions.CustomGenerator("overworld").setModGenerationBaseDimension(0).setBuildVanillaSurfaces(true).setGenerateVanillaStructures(true)
 MatrixDimension.setCloudColor(0, 0, 0)
 // MatrixDimension.setFogColor(0, 0, 0)
@@ -55,9 +56,17 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 var Matrix = {
-    ticks: 0,
-    time: 0,
+    data: {
+        ticks: 0,
+        time: 0,
+        first: true,
+    },
     particles: [],
+    dialog: [
+        "Wake up, Neo...",
+        "The Matrix has you...",
+        "Follow the white rabbit."
+    ],
     commands: {
         noclip: null, nightvision: null, slow: null,
         fly: null, gm: null, speed: null,
@@ -76,7 +85,8 @@ var Matrix = {
         }
     },
     tick() {
-        this.ticks++
+        this.data.ticks++
+        this.startDialog()
     },
 
     add(coords, radius, count) {
@@ -88,6 +98,24 @@ var Matrix = {
                 Particles.addParticle(this.particles[randomInt(0, this.particles.length - 1)], x + Math.random() * 1.382, y + i, z + Math.random() * 1.382, 0, -0.35, 0)
             }
         }
+    },
+    startDialog() {
+        if (this.data.first == true)
+            switch (this.data.ticks) {
+                case 20: Game.message(Native.Color.GREEN + this.dialog[0]); break
+                case 60: Game.message(Native.Color.GREEN + this.dialog[1]); break
+                case 100:
+                    Game.message(Native.Color.GREEN + this.dialog[2])
+                    let c = Player.getPosition()
+                    Entity.spawn(c.x + randomInt(5, 15), c.y + 2, c.z + randomInt(5, 15), Native.EntityType.RABBIT)
+                    this.data.first == false
+                    this.data.ticks == 0
+                    break
+
+                default:
+                    break
+            }
+
     }
 }
 
@@ -95,10 +123,11 @@ Matrix.regParticles(["m0", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9",
 
 Callback.addCallback("tick", function () {
     let coords = Entity.getPosition(Player.get())
-    if (Player.getDimension() == 1200) {
+    if (Player.getDimension() == 1997) {
         if (World.getThreadTime() % 4 === 0) {
             World.setWorldTime(22000)
             Matrix.add(coords, 5, 4)
+            Matrix.tick()
         }
     }
 })
@@ -107,7 +136,7 @@ Callback.addCallback("NativeCommand", function (str) {
     str = str.split(" ")
     let cmdName = str[0]
     let cmdArg = str[1]
-    if (str[0].replace("/", '') in Matrix.commands)
+    if (str[0].replace("/", "") in Matrix.commands)
         Game.prevent()
 
     switch (cmdName) {
@@ -115,7 +144,7 @@ Callback.addCallback("NativeCommand", function (str) {
             Player.setAbility(Native.PlayerAbility.NOCLIP, Boolean(cmdArg || false) || false)
             break
         case "/nightvision":
-            cmdArg ?
+            Boolean(cmdArg) == true ?
                 Entity.addEffect(Player.get(), Native.PotionEffect.nightVision, 0, 200000) :
                 Entity.clearEffect(Player.get(), Native.PotionEffect.nightVision)
             break
@@ -156,9 +185,9 @@ Block.createBlock("matrix_tel", [{ name: "Matrix Telephone", textures: [["cauldr
 var telMesh = new RenderMesh()
 telMesh.setBlockTexture("tel", 0)
 telMesh.importFromFile(__dir__ + "assets/mod_assets/models/tel.obj", "obj", { translate: [1, 0, 1], scale: [1 / 16, 1 / 16, 1 / 16] })
-let icrender = new ICRender.Model()
-icrender.addEntry(new BlockRenderer.Model(telMesh))
-BlockRenderer.setStaticICRender(BlockID.matrix_tel, 0, icrender)
+var icrender1 = new ICRender.Model()
+icrender1.addEntry(new BlockRenderer.Model(telMesh))
+BlockRenderer.setStaticICRender(BlockID.matrix_tel, 0, icrender1)
 
 Callback.addCallback("ItemUse", function (coords, item, block) {
     if (block.id == BlockID.matrix_tel) {
@@ -182,6 +211,72 @@ function getBlockRotation(isFull) {
     if (isFull) return rotation + 2
     return rotation
 }
-Block.registerPlaceFunction(BlockID.matrix_tel, function () {
 
+
+
+IDRegistry.genBlockID("matrix_dish")
+Block.createBlock("matrix_dish", [{ name: "Dish", textures: [["cauldron_inner", 0]], inCreative: true }])
+var dishMesh = new RenderMesh()
+dishMesh.setBlockTexture("dish", 0)
+dishMesh.importFromFile(__dir__ + "assets/mod_assets/models/dish.obj", "obj", { translate: [1, 0, 1], scale: [1 / 16, 1 / 16, 1 / 16] })
+var icrender2 = new ICRender.Model()
+icrender2.addEntry(new BlockRenderer.Model(dishMesh))
+BlockRenderer.setStaticICRender(BlockID.matrix_dish, 0, icrender2)
+Block.setShape(BlockID.matrix_dish, 0, 0, 0, 1, 2.5 / 16, 1)
+
+
+TileEntity.registerPrototype(BlockID.matrix_dish, {
+    defaultValues: {
+        item: { id: 0, count: 0, data: 0 },
+        itemAnim: null
+    },
+    init() {
+        this.data.itemAnim = new Animation.Item(this.x + 0.5, this.y + 0.2, this.z + 0.5)
+        this.updateItem()
+    },
+    click(id, count, data, coords) {
+        let item = this.data.item
+        if (item.id == 0 && item.count == 0 && id !== 0 && count != 0) {
+            this.data.item = {
+                id: id, count: 1, data: data
+            }
+            let carr = Player.getCarriedItem()
+            Player.setCarriedItem(carr.id, carr.count - 1, carr.data)
+        } else {
+            this.data.item = {
+                id: 0, count: 0, data: 0
+            }
+            World.drop(coords.x + 0.5, coords.y + 0.2, coords.z + 0.5, item.id, item.count, item.data)
+        }
+        this.updateItem()
+    },
+    updateItem() {
+        let item = this.data.item
+        this.data.itemAnim.load()
+        this.data.itemAnim.describeItem({
+            id: item.id, count: 1, data: item.data, notRandomize: true, rotation: [Math.PI / 2, 0, 0]
+        })
+        this.data.itemAnim.refresh()
+        this.data.itemAnim.updateRender()
+    }
 })
+
+
+Saver.addSavesScope("PouchScope",
+    function read(scope) {
+        Matrix.data = scope.data || {
+            ticks: 0,
+            time: 0,
+            first: true,
+        }
+    },
+    function save() {
+        return {
+            data: Matrix.data
+        }
+    }
+)
+Callback.addCallback("GenerateCustomDimensionChunk", function (chunkX, chunkZ, random, dimID) {
+    if (dimID != 1997) return
+    GenerationUtils.findSurface(chunkX, y, chunkZ)
+});
