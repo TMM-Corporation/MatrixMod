9eb2ad5522f6ad38bedfdff9fe4a913d384e6495
ToxesFoxes <toxes_foxes@outlook.com>
2020-10-15 21:12:30 +0600

    Changed pellets recipes

--- src/dev/Pellets.js
+++ src/dev/Pellets.js
@@ -2,14 +2,15 @@
 IDRegistry.genItemID("blue_pellet")
 IDRegistry.genItemID("red_pellet")
 
-Item.createFoodItem("blue_pellet", "Blue pellet", { name: "blue_pellet" }, { stack: 8, food: 4 })
-Item.createFoodItem("red_pellet", "Red pellet", { name: "red_pellet" }, { stack: 8, food: 4 })
+Item.createFoodItem("blue_pellet", "Blue pellet", { name: "blue_pellet" }, { stack: 8, food: 8 })
+Item.createFoodItem("red_pellet", "Red pellet", { name: "red_pellet" }, { stack: 8, food: 16 })
 
 Recipes.addShaped({ id: ItemID.blue_pellet, count: 1, data: 0 }, [
-    "ax",
-    "xa",
-    "ax"
-], ['x', 265, 0, 'a', 266, 0])
+    "abc"
+], ['a', 322, 0, 'b', 396, 0, 'c', 351, 18])
+Recipes.addShaped({ id: ItemID.red_pellet, count: 1, data: 0 }, [
+    "abc", 'de'
+], ['a', 322, 0, 'b', 396, 0, 'c', 351, 1, 'd', 382, 0, 'e', 368, 0])
 
 
 Callback.addCallback("FoodEaten", function (food, ratio, player) {
@@ -17,12 +18,10 @@ Callback.addCallback("FoodEaten", function (food, ratio, player) {
     var clientPlayer = client.getPlayerUid()
     switch (Entity.getCarriedItem(clientPlayer).id) {
         case ItemID.blue_pellet:
-            if (Entity.getDimension(clientPlayer) == 0) {
-                Entity.addEffect(clientPlayer, Native.PotionEffect.absorption, 5, 6000)
-                Entity.addEffect(clientPlayer, Native.PotionEffect.damageBoost, 2, 6000)
-                Entity.addEffect(clientPlayer, Native.PotionEffect.damageResistance, 2, 6000)
-                Entity.addEffect(clientPlayer, Native.PotionEffect.digSpeed, 2, 6000)
-            }
+            Entity.addEffect(clientPlayer, Native.PotionEffect.absorption, 5, 6000)
+            Entity.addEffect(clientPlayer, Native.PotionEffect.damageBoost, 2, 6000)
+            Entity.addEffect(clientPlayer, Native.PotionEffect.damageResistance, 2, 6000)
+            Entity.addEffect(clientPlayer, Native.PotionEffect.digSpeed, 2, 6000)
             break
         case ItemID.red_pellet:
             Entity.addEffect(clientPlayer, Native.PotionEffect.regeneration, 5, 12000)
